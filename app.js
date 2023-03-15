61466ff0e985414e9767a7028adf401f486fd02e
keemhnb <126596917+keemhnb@users.noreply.github.com>
2023-03-15 22:48:38 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const calculator = {
+    plus : function(a,b){
+        console.log(a+b)
+    },
+    minus : function(a,b){
+        console.log(a-b)
+    },
+    devide : function(a,b){
+        console.log(a/b)
+    },
+    powerof : function(a,b){
+        console.log(a**b)
+    }
+}
+
+calculator.plus(4,14)
+calculator.minus(4,14)
+calculator.devide(14,4)
+calculator.powerof(10,4)
